c3dec1d7d80df732a7a257459e1dc347bbc9aeae
Marcos Almonacid <marcos@inakanetworks.com>
2012-07-07 01:32:02 -0300

    records.hrl file

---
+++ include/records.hrl
@@ -0,0 +1,16 @@
+-record(pinger,{ id :: pos_integer(),
+                 name :: string(),
+                 type :: atom() ,
+                 user_id :: pos_integer,
+                 end_point :: string(),
+                 frecuency :: pos_integer()
+                }).
+-record(subscription,{ id             :: pos_integer(),
+                       type           :: email | tweet,
+                       user_id        :: pos_integer(),
+                       pinger_id      :: pos_integer(),
+                       down_time      :: pos_integer(),
+                       notify_when_up :: boolean()}).
+-record(event,{ type      :: pinger_down | pinger_up,
+                pinger_id :: pos_integer(),
+                down_time :: pos_integer()}).
