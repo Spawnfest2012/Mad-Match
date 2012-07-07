c21243a92aef9d9f12a2f2916f68cb3f15154e3d
Gustavo Chain <gustavo.chain@gmail.com>
2012-07-07 17:55:49 -0300

    user crypto

--- src/migration.sql
+++ src/migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE  `users` (
  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
  `name` VARCHAR( 50 ) NOT NULL ,
  `email` VARCHAR( 50 ) NOT NULL ,
- `password` VARCHAR( 50 ) NOT NULL ,
+ `password` VARCHAR( 256 ) NOT NULL ,
  `tagline` VARCHAR( 50 ) NULL ,
  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ,
 UNIQUE (`email`)
--- src/ping_user_db.erl
+++ src/ping_user_db.erl
@@ -6,15 +6,29 @@
 -include_lib("deps/emysql/include/emysql.hrl").
 
 
--export([find/1,create/4]).
+-export([find/1,find/2,create/4]).
 
 -spec find(integer()) -> [#user{}].
-find(Id) -> 
-Result = ping_db:find(?USER_TABLE,[{where,[{id,lists:flatten(io_lib:format("~p",[Id]))}]}]),
-  emysql_util:as_record(
-		Result, user, record_info(fields, user)).
-  
+find(Id) ->
+  find(Id, undefined).
+
+find(Id, Password) ->
+  Result = case Password of
+    undefined ->
+      ping_db:find(?USER_TABLE,[{where,[{id,lists:flatten(io_lib:format("~p",[Id]))}]}]);
+    _ ->
+      CryptoPass = erlsha2:sha512(list_to_binary(Password)),
+      HexPass = hmac:hexlify(CryptoPass),
+      io:format("Hex: ~p\n", [HexPass]),
+      ping_db:find(?USER_TABLE,[{where,[
+              {id,lists:flatten(io_lib:format("~p",[Id]))},
+              {password, HexPass}
+              ]}])
+  end,
+  emysql_util:as_record(Result, user, record_info(fields, user)).
 
 -spec create(string(),string(),string(),string()) -> {ok,pos_integer()}.
 create(Name,Email,Password,Tagline) -> 
-  Result = ping_db:create(?USER_TABLE,[{name,Name},{email,Email},{password,Password},{tagline,Tagline}]).
+  CryptoPass = erlsha2:sha512(list_to_binary(Password)),
+  HexPass = hmac:hexlify(CryptoPass),
+  Result = ping_db:create(?USER_TABLE,[{name,Name},{email,Email},{password,HexPass},{tagline,Tagline}]).
