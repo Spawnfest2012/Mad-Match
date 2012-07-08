d69fc93203207c4d4f7502a87e4748bd53f921d2
C B DePue <chad@inakanetworks.com>
2012-07-08 18:22:25 -0300

    you can edit your account

--- src/controllers/ping_edit_controller.erl
+++ src/controllers/ping_edit_controller.erl
@@ -9,7 +9,9 @@
 
 render(Req,Session) ->
   case ping_session:is_logged_in(Session) of
-    true -> edit_dtl:render(Session ++ [{button_label,"Edit My Account"}]);
+    true -> 
+      [Urecord] = ping_user_db:find(proplists:get_value(uid,Session)),
+      edit_dtl:render(Session ++ [{password_label,"Re-enter Password to Save"},{button_label,"Edit My Account"}] ++ ping_utils:record_to_proplist(Urecord));
     false -> ?BASE_MODULE:redirect(301, "/", Req)
   end.
 
--- src/controllers/ping_signup_controller.erl
+++ src/controllers/ping_signup_controller.erl
@@ -7,6 +7,6 @@
 -export([render/2]).
 
 render(Req,Session) ->
-  signup_dtl:render(Session ++ [{button_label,"I'm pingterested, sign me up!"}]).
+  signup_dtl:render(Session ++ [{name,""},{email,""},{twitter,""},{tagline,""},{password_label,"Enter Password to Save"},{button_label,"I'm pingterested, sign me up!"}]).
 
 
