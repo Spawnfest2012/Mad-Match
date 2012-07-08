037181a787c636519b78b5370ab6ad9097b3269c
C B DePue <chad@inakanetworks.com>
2012-07-08 12:08:07 -0300

    me page is done except manuel needs to give us a query

--- src/controllers/ping_reel_controller.erl
+++ src/controllers/ping_reel_controller.erl
@@ -11,7 +11,6 @@
 
 render(Req,Session) ->
   {Page, _} = cowboy_http_req:qs_val(<<"page">>,Req),
-  lager:warning("thepageis : ~p ~n",[Page]),
 
   %% use extends
   Pingers = lists:map(fun(P) -> Pl = ping_utils:record_to_proplist(P), {ok,Html} = pinger_dtl:render(Pl), Html end, ping_pinger_db:firehose(Page,?PER_PAGE)),
